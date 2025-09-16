a3b073822c7e6fdf0ada8526cd011f3c4b12802d
Shrey Pandey <shreypandey22005@gmail.com>
2025-09-16 10:34:02 +0530

    navbar and intial design

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 
-const navbar = () => {
+const Navbar = () => {
   return (
-    <nav>
+    <nav className="navbar bg-gray-600 flex justify-between items-center px-7 py-4">
+      <div className="logo font-bold text-xl">&lt;Logo<span className='text-blue-300'>Pass/&gt;</span></div>
         <ul>
-            <li>
-                <a href="/">Home</a>
-                <a href=""></a>
-                <a href=""></a>
-                <a href=""></a>
+            <li className='flex gap-4 '>
+                <a className='hover:font-bold hover:text-blue-300' href="/">Home</a>
+                <a className='hover:font-bold hover:text-blue-300' href="#">About</a>
+                <a className='hover:font-bold hover:text-blue-300' href="#">Contact</a>
+                <a className='hover:font-bold hover:text-blue-300' href="#">More</a>
             </li>
         </ul>
     </nav>
   )
 }
 
-export default navbar
+export default Navbar
